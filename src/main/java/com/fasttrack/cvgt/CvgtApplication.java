f54e72bbfbba4f5ba09868a1d9bc03fc4ba1ea98
jurchis <jurchis@users.noreply.github.com>
2020-01-26 17:42:33 +0200

    Initial commit - Project Structure

---
+++ src/main/java/com/fasttrack/cvgt/CvgtApplication.java
@@ -0,0 +1,13 @@
+package com.fasttrack.cvgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fasttrack/cvgt/CvgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasttrack.cvgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
