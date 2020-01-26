f4624ff425ced5c94a7bf0c39a5824f9bec4c49d
jurchis <jurchis@users.noreply.github.com>
2020-01-26 20:19:18 +0200

    switched properties to .yml file type

---
+++ src/main/java/com/fasttrack/cvgt/persistance/MediaRepository.java
@@ -0,0 +1,8 @@
+package com.fasttrack.cvgt.persistance;
+
+import com.fasttrack.cvgt.domain.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaRepository extends JpaRepository<Media, Long> {
+
+}
