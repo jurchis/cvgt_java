5e3a546bcfdf07cd3dd26d48d59555b091a60d09
jurchis <jurchis@users.noreply.github.com>
2020-01-27 19:45:37 +0200

    added media service and repository

---
+++ src/main/java/com/fasttrack/cvgt/transfer/SaveMediaRequest.java
@@ -0,0 +1,31 @@
+package com.fasttrack.cvgt.transfer;
+
+public class SaveMediaRequest {
+    private String description;
+    private String name;
+    private String mediaUrl;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMediaUrl() {
+        return mediaUrl;
+    }
+
+    public void setMediaUrl(String mediaUrl) {
+        this.mediaUrl = mediaUrl;
+    }
+}
