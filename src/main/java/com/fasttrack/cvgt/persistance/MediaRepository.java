2a683bd6e33504f2266478652042f7271a0d2eb5
jurchis <jurchis@users.noreply.github.com>
2020-02-04 16:29:05 +0200

    created all classes and tested the app with success. Also the app has been checked from the web and is returning the response to the client; the UI is also ready and now working to integrate the UI with the Backend - WIP

--- src/main/java/com/fasttrack/cvgt/persistance/MediaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fasttrack.cvgt.persistance;
-
-import com.fasttrack.cvgt.domain.Media;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MediaRepository extends JpaRepository<Media, Long> {
-
-}
